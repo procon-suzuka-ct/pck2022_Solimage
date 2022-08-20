6c3e87387e2250b73954cc43823fc3009d7355ea
Hideki Tanaka <work@tuxsnct.com>
2022-08-20 09:38:25 +0900

    add: user name dialog and group dialog

--- App/lib/routes/parent/parent.dart
+++ App/lib/routes/parent/parent.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animated_dialog/flutter_animated_dialog.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:solimage/states/parent.dart';
@@ -32,19 +33,53 @@ class ParentScreen extends ConsumerWidget {
           const ListTile(
               title: Text('ユーザー情報',
                   style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold))),
-          const Card(
-              child: ListTile(title: Text('名前'), trailing: Icon(Icons.edit))),
+          Card(
+              child: ListTile(
+                  title: const Text('名前'),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.edit),
+                    onPressed: () => showAnimatedDialog(
+                        context: context,
+                        animationType: DialogTransitionType.fadeScale,
+                        barrierDismissible: true,
+                        builder: (context) => const UserNameDialog()),
+                  ))),
           const ListTile(
               title: Text('グループ',
                   style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold))),
-          const Card(
-              child:
-                  ListTile(title: Text('グループA'), trailing: Icon(Icons.info))),
-          const Card(
-              child:
-                  ListTile(title: Text('グループB'), trailing: Icon(Icons.info))),
-          const Card(
-              child: ListTile(title: Text('グループC'), trailing: Icon(Icons.info)))
+          Card(
+              child: ListTile(
+                  title: const Text('グループA'),
+                  trailing: IconButton(
+                      icon: const Icon(Icons.info),
+                      onPressed: () => showAnimatedDialog(
+                          context: context,
+                          animationType: DialogTransitionType.fadeScale,
+                          barrierDismissible: true,
+                          builder: (context) =>
+                              const GroupDialog(groupName: 'グループA'))))),
+          Card(
+              child: ListTile(
+                  title: const Text('グループB'),
+                  trailing: IconButton(
+                      icon: const Icon(Icons.info),
+                      onPressed: () => showAnimatedDialog(
+                          context: context,
+                          animationType: DialogTransitionType.fadeScale,
+                          barrierDismissible: true,
+                          builder: (context) =>
+                              const GroupDialog(groupName: 'グループB'))))),
+          Card(
+              child: ListTile(
+                  title: const Text('グループC'),
+                  trailing: IconButton(
+                      icon: const Icon(Icons.info),
+                      onPressed: () => showAnimatedDialog(
+                          context: context,
+                          animationType: DialogTransitionType.fadeScale,
+                          barrierDismissible: true,
+                          builder: (context) =>
+                              const GroupDialog(groupName: 'グループC'))))),
         ]
       }
     ];
@@ -84,4 +119,40 @@ class ParentScreen extends ConsumerWidget {
               ])
             : null);
   }
+}
+
+class UserNameDialog extends StatelessWidget {
+  const UserNameDialog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => AlertDialog(
+        title: const Text('ユーザー名'),
+        content: const TextField(
+            decoration:
+                InputDecoration(hintText: '名前を入力してください', labelText: '名前')),
+        actions: <Widget>[
+          TextButton(
+              child: const Text('はい'),
+              onPressed: () => Navigator.of(context).pop()),
+          TextButton(
+              child: const Text('いいえ'),
+              onPressed: () => Navigator.of(context).pop()),
+        ],
+      );
+}
+
+class GroupDialog extends StatelessWidget {
+  const GroupDialog({Key? key, required this.groupName}) : super(key: key);
+
+  final String groupName;
+
+  @override
+  Widget build(BuildContext context) => AlertDialog(
+        title: Text(groupName),
+        actions: <Widget>[
+          TextButton(
+              child: const Text('閉じる'),
+              onPressed: () => Navigator.of(context).pop()),
+        ],
+      );
 }
