c7268e323224d9d5f8dc7b7425dce01843eab9b2
Hideki Tanaka <work@tuxsnct.com>
2022-08-23 12:55:51 +0900

    fix: invalid widget

--- App/lib/routes/parent/parent.dart
+++ App/lib/routes/parent/parent.dart
@@ -4,7 +4,7 @@ import 'package:go_router/go_router.dart';
 import 'package:solimage/routes/parent/history.dart';
 import 'package:solimage/routes/parent/settings.dart';
 
-final tabIndexProvider = StateProvider.autoDispose((ref) => 0);
+final tabIndexProvider = StateProvider((ref) => 0);
 
 class ParentScreen extends ConsumerWidget {
   const ParentScreen({Key? key}) : super(key: key);
@@ -35,7 +35,7 @@ class ParentScreen extends ConsumerWidget {
         ),
         body: Container(
             margin: const EdgeInsets.all(10.0),
-            child: ListView(children: parentScreenTabs[tabIndex]['children'])),
+            child: parentScreenTabs[tabIndex]['child']),
         bottomNavigationBar: NavigationBar(
             destinations: parentScreenTabs
                 .map((element) => NavigationDestination(
