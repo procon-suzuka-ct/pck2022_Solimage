3edd68e989e61c6229a4f4b76646713ac6d07f43
Hideki Tanaka <work@tuxsnct.com>
2022-09-06 15:50:56 +0900

    change: providers from state provider to stream provider

--- App/lib/states/auth.dart
+++ App/lib/states/auth.dart
@@ -1,4 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:solimage/utils/auth.dart';
 
-final authProvider = StateProvider((ref) => Auth().currentUser());
+final authProvider = StreamProvider((ref) => Auth().authStateChanges);
--- App/lib/states/user.dart
+++ App/lib/states/user.dart
@@ -2,7 +2,23 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:solimage/states/auth.dart';
 import 'package:solimage/utils/classes/user.dart';
 
-final userProvider = StateProvider((ref) {
-  final auth = ref.watch(authProvider);
-  return auth != null ? AppUser(uid: auth.uid, name: auth.displayName!) : null;
+final userProvider = StreamProvider((ref) async* {
+  final auth = await ref.watch(authProvider.future);
+
+  if (auth != null) {
+    var user = await AppUser.getUser(auth.uid);
+
+    if (user == null) {
+      user = AppUser(uid: auth.uid, name: auth.displayName!);
+      await user.save();
+    }
+    yield user;
+  } else {
+    yield null;
+  }
 });
+
+final photoURLProvider = StateProvider(
+    (ref) => ref.watch(authProvider.select((value) => value.value?.photoURL)));
+final nameProvider = StateProvider(
+    (ref) => ref.watch(userProvider.select((value) => value.value?.name)));
