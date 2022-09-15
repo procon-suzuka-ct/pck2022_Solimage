86318226e0f4d2ee87724cfc3c0c0a9dab61cd7f
Hideki Tanaka <work@tuxsnct.com>
2022-09-16 03:43:51 +0900

    add: group participation feature

--- App/lib/routes/parent/profile.dart
+++ App/lib/routes/parent/profile.dart
@@ -8,17 +8,15 @@ import 'package:solimage/utils/auth.dart';
 import 'package:solimage/utils/classes/group.dart';
 import 'package:solimage/utils/classes/user.dart';
 
-final _photoURLProvider = StateProvider.autoDispose(
-    (ref) => ref.watch(authProvider.select((value) => value.value?.photoURL)));
-final _nameProvider = StateProvider.autoDispose(
-    (ref) => ref.watch(userProvider.select((value) => value.value?.name)));
-final _groupsProvider = FutureProvider.autoDispose((ref) async {
-  final groupIds =
-      ref.watch(userProvider.select((value) => value.value?.groups));
-  return groupIds != null
-      ? await Future.wait(groupIds.map((groupId) => Group.getGroup(groupId)))
-      : null;
-});
+final _photoURLProvider = FutureProvider.autoDispose(
+    (ref) => ref.watch(authProvider.future).then((auth) => auth?.photoURL));
+final _nameProvider = FutureProvider.autoDispose(
+    (ref) => ref.watch(userProvider.future).then((user) => user?.name));
+final _groupsProvider = FutureProvider.autoDispose((ref) async =>
+    await Future.wait((await ref
+            .watch(userProvider.future)
+            .then((user) => user?.groups ?? []))
+        .map((groupID) => Group.getGroup(groupID))));
 
 class ProfileScreen extends ConsumerWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -41,14 +39,17 @@ class ProfileScreen extends ConsumerWidget {
               verticalDirection: VerticalDirection.down,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                if (photoURL != null)
-                  Container(
-                      margin: const EdgeInsets.all(10.0),
-                      child: CircleAvatar(
-                          radius: 64.0,
-                          backgroundImage: NetworkImage(photoURL))),
-                if (name != null)
-                  Text('$nameさん', style: const TextStyle(fontSize: 20.0))
+                photoURL.maybeWhen(
+                    data: (data) => Container(
+                        margin: const EdgeInsets.all(10.0),
+                        child: CircleAvatar(
+                            radius: 64.0,
+                            backgroundImage: NetworkImage(data!))),
+                    orElse: () => const CircularProgressIndicator()),
+                name.maybeWhen(
+                    data: (data) =>
+                        Text('$dataさん', style: const TextStyle(fontSize: 20.0)),
+                    orElse: () => const CircularProgressIndicator())
               ])),
       Card(
           child: ListTile(
@@ -84,21 +85,19 @@ class ProfileScreen extends ConsumerWidget {
                 child: const Text('参加'))
           ])),
       ...groups.maybeWhen(
-          data: (data) => (data != null
-              ? data
-                  .map((group) => group != null
-                      ? Card(
-                          child: ListTile(
-                              title: Text(group.groupName),
-                              trailing: const Icon(Icons.info),
-                              onTap: () => showDialog(
-                                  barrierDismissible: false,
-                                  context: context,
-                                  builder: (context) =>
-                                      GroupDetailDialog(group: group))))
-                      : const SizedBox.shrink())
-                  .toList()
-              : const [SizedBox.shrink()]),
+          data: (data) => data
+              .map((group) => group != null
+                  ? Card(
+                      child: ListTile(
+                          title: Text(group.groupName),
+                          trailing: const Icon(Icons.info),
+                          onTap: () => showDialog(
+                              barrierDismissible: false,
+                              context: context,
+                              builder: (context) =>
+                                  GroupDetailDialog(group: group))))
+                  : const SizedBox.shrink())
+              .toList(),
           orElse: () => const [Center(child: CircularProgressIndicator())]),
       const ListTile(
           title: Text('アクセス履歴',
@@ -115,7 +114,7 @@ class NameEditDialog extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final name = ref.watch(_nameProvider);
-    final controller = TextEditingController(text: name);
+    final controller = TextEditingController(text: name.value);
 
     return AlertDialog(
       title: const Text('名前'),
@@ -126,7 +125,7 @@ class NameEditDialog extends ConsumerWidget {
         TextButton(
             child: const Text('OK'),
             onPressed: () {
-              if (controller.text.isNotEmpty && controller.text != name) {
+              if (controller.text.isNotEmpty && controller.text != name.value) {
                 user!.setData(user!.uid, controller.text);
                 ref.refresh(_nameProvider);
                 user!.save();
@@ -211,24 +210,23 @@ class GroupCreationDialog extends ConsumerWidget {
           decoration: const InputDecoration(
               labelText: 'グループ名', hintText: 'グループ名を入力してください')),
       actions: <Widget>[
-        Builder(
-            builder: (context) => TextButton(
-                child: const Text('OK'),
-                onPressed: () async {
-                  if (controller.text.isNotEmpty) {
-                    Navigator.of(context).pop();
-                    final group = Group(groupName: controller.text);
-                    await group.init();
-                    user?.groups.add(group.groupID);
-                    await user?.save();
-                    group.addMember(user!.uid);
-                    await group.save();
-                    parentRef.refresh(_groupsProvider);
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('グループ名を入力してください')));
-                  }
-                })),
+        TextButton(
+            child: const Text('OK'),
+            onPressed: () async {
+              if (controller.text.isNotEmpty) {
+                Navigator.of(context).pop();
+                final group = Group(groupName: controller.text);
+                await group.init();
+                user?.groups.add(group.groupID);
+                await user?.save();
+                group.addMember(user!.uid);
+                await group.save();
+                parentRef.refresh(_groupsProvider);
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('グループ名を入力してください')));
+              }
+            }),
         TextButton(
             child: const Text('キャンセル'),
             onPressed: () => Navigator.of(context).pop()),
@@ -245,55 +243,42 @@ class GroupParticipationDialog extends ConsumerWidget {
   final WidgetRef parentRef;
   final AppUser? user;
   final _controller = TextEditingController();
-  final idProvider = StateProvider<String?>((ref) => null);
-  late final getGroupProvider = FutureProvider((ref) async {
-    final id = ref.read(idProvider);
-    return id != null ? await Group.getGroup(int.parse(id)) : null;
-  });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final groupRef = ref.watch(getGroupProvider);
-
-    return AlertDialog(
-      title: const Text('グループに参加'),
-      content: TextField(
-          controller: _controller,
-          decoration: const InputDecoration(
-              labelText: 'グループID', hintText: 'グループIDを入力してください'),
-          keyboardType: TextInputType.number,
-          onChanged: (value) {
-            ref.read(idProvider.notifier).state = value;
-          }),
-      actions: <Widget>[
-        TextButton(
-            child: const Text('OK'),
-            onPressed: () {
-              if (_controller.text.isNotEmpty) {
-                groupRef.maybeWhen(
-                    data: (group) async {
-                      Navigator.of(context).pop();
-                      if (group != null) {
-                        user!.groups.add(group.groupID);
-                        await user!.save();
-                        group.addMember(user!.uid);
-                        await group.save();
-                        parentRef.refresh(_groupsProvider);
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text('そのグループは存在しません')));
-                      }
-                    },
-                    orElse: () => null);
-              } else {
-                ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('グループIDを入力してください')));
-              }
-            }),
-        TextButton(
-            child: const Text('キャンセル'),
-            onPressed: () => Navigator.of(context).pop()),
-      ],
-    );
-  }
+  Widget build(BuildContext context, WidgetRef ref) => AlertDialog(
+        title: const Text('グループに参加'),
+        content: TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+                labelText: 'グループID', hintText: 'グループIDを入力してください'),
+            keyboardType: TextInputType.number),
+        actions: <Widget>[
+          TextButton(
+              child: const Text('OK'),
+              onPressed: () async {
+                if (_controller.text.isNotEmpty) {
+                  final group = Group.getGroup(int.parse(_controller.text));
+                  Navigator.of(context).pop();
+                  group.then((value) async {
+                    if (value != null) {
+                      user!.groups.add(value.groupID);
+                      await user!.save();
+                      value.addMember(user!.uid);
+                      await value.save();
+                      parentRef.refresh(_groupsProvider);
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('そのグループは存在しません')));
+                    }
+                  });
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('グループIDを入力してください')));
+                }
+              }),
+          TextButton(
+              child: const Text('キャンセル'),
+              onPressed: () => Navigator.of(context).pop()),
+        ],
+      );
 }
