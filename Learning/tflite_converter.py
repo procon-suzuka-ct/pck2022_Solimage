870b90fa28b74e5ef52a03510ee39884690e1c12
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-09-22 00:23:15 +0900

    update: 8bit quantization

--- Learning/tflite_converter.py
+++ Learning/tflite_converter.py
@@ -6,10 +6,15 @@ import tensorflow_addons as tfa
 dirPath = "tmp/model"
 filename = "model.h5"
 modelPath = "model.tflite"
+quantize_mode =  'int8'
 base = os.path.dirname(os.path.abspath(__file__))
 os.makedirs(os.path.join(base, dirPath), exist_ok = True)
 filePath = os.path.join(base, filename)
 model = keras.models.load_model(filePath, custom_objects = {"rrelu": tfa.activations.rrelu})
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS, tf.lite.OpsSet.SELECT_TF_OPS]
+converter.allow_custom_ops = True
 tflite_model = converter.convert()
 open(os.path.join(base, dirPath, modelPath), "wb").write(tflite_model)
