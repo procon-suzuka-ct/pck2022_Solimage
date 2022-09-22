ab2bcf1133e92542f17c4902cba4d51732c0145f
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-09-22 14:28:04 +0900

    update: add default constructor

--- App/lib/utils/imageProcess/classifier.dart
+++ App/lib/utils/imageProcess/classifier.dart
@@ -36,6 +36,13 @@ class Classifier {
 
   final NormalizeOp _preProcessNormalizeOp = NormalizeOp(0, 1);
 
+  Classifier() {
+    _interpreterOptions = InterpreterOptions();
+    _interpreterOptions.threads = 1;
+
+    loadModel();
+  }
+
   TensorImage preProcess(TensorImage inputImage) {
     int cropSize = min(inputImage.height, inputImage.width);
     return ImageProcessorBuilder()
