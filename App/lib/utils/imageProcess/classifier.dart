62e6ee22a8a0de3cd5a03afe8f3b60b1163f5345
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-09-22 14:15:25 +0900

    hotfix: constructor forget load model

--- App/lib/utils/imageProcess/classifier.dart
+++ App/lib/utils/imageProcess/classifier.dart
@@ -15,7 +15,12 @@ class Classifier {
   //singleton
   static final Classifier _singleton = Classifier._internal();
   static Classifier get instance => _singleton;
-  Classifier._internal();
+  Classifier._internal() {
+    _interpreterOptions = InterpreterOptions();
+    _interpreterOptions.threads = 1;
+
+    loadModel();
+  }
 
   late Interpreter _interpreter;
   late InterpreterOptions _interpreterOptions;
