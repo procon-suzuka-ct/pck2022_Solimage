f325bbc191d79573e41ad4097409653b1f43ab0e
Hideki Tanaka <work@tuxsnct.com>
2022-09-28 02:11:13 +0900

    remove: unnecessary import

--- App/lib/components/child/standby.dart
+++ App/lib/components/child/standby.dart
@@ -1,5 +1,4 @@
 import 'package:camera/camera.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:image/image.dart' as image;
