29af4ca213bcd6ee9f859c8dd04e84dc7f771b88
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-09-28 03:26:43 +0900

    update: save method

--- App/lib/utils/classes/history.dart
+++ App/lib/utils/classes/history.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class History {
   final String _uid;
   final String _word;
-  final String _imageDataUrl;
+  late String _imageDataUrl;
   final int _year;
   final int _month;
   final int _day;
@@ -15,8 +18,7 @@ class History {
   int get month => _month;
   int get day => _day;
 
-  History(this._uid, this._word, this._imageDataUrl, this._year, this._month,
-      this._day);
+  History(this._uid, this._word, this._year, this._month, this._day);
 
   History.fromJson(Map<String, Object?> json)
       : _uid = json['uid'] as String,
@@ -45,7 +47,7 @@ class History {
   }
 
   //ユーザー情報をFirestoreに保存する
-  Future<void> save() async {
+  Future<void> _save() async {
     await _getRef(uid).doc(word).set(toJson());
   }
 
@@ -64,5 +66,18 @@ class History {
     for (var doc in snapshot.docs) {
       await doc.reference.delete();
     }
+  }
+
+  Future<String> save(String imagePath) async {
+    final ref = FirebaseStorage.instance
+        .ref()
+        .child('histories/$uid/$word${DateTime.now()}.jpg');
+    File file = File(imagePath);
+    final task = ref.putFile(file);
+    final snapshot = await task;
+    final url = await snapshot.ref.getDownloadURL();
+    _imageDataUrl = url;
+    await _save();
+    return url;
   }
 }
