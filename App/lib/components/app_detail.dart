b64cc5ff304ce479d83e7fc56dabe76e08ea9395
Hideki Tanaka <work@tuxsnct.com>
2022-10-03 02:18:39 +0900

    update: remove discontinued package

--- App/lib/components/app_detail.dart
+++ App/lib/components/app_detail.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 Future<void> showAppDetailDialog(BuildContext context) async {
   PackageInfo packageInfo = await PackageInfo.fromPlatform();
