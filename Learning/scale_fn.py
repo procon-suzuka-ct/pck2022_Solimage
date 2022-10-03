6afa6734f951c6deaa0bc3c9a2ffe685c586c841
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-10-04 00:49:48 +0900

    update: add label

--- Learning/scale_fn.py
+++ Learning/scale_fn.py
@@ -1,8 +1,12 @@
 import tensorflow as tf, numpy as np, matplotlib.pyplot as plt
-scale_fn = lambda x: tf.math.abs(tf.math.cos(x * np.pi / 6) - tf.math.sin(x * np.pi / 12)) / (tf.math.sqrt(tf.math.log(x + 1)) + tf.math.log(x + 1) + tf.math.abs(tf.sin(x * np.pi / 6)))
+scale_fn = \
+  lambda x: tf.math.abs(tf.math.cos(x * np.pi / 6) - tf.math.sin(x * np.pi / 12)) /\
+            (tf.math.sqrt(tf.math.log(x + 1)) + tf.math.log(x + 1) + tf.math.abs(tf.sin(x * np.pi / 6)))
 
 x = np.arange(0, 500, 1.)
 y = scale_fn(x)
 
 plt.plot(x, y)
+plt.xlabel("steps")
+plt.ylabel("learning rate")
 plt.show()
