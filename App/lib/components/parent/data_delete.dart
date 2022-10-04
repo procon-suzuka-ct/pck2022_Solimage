a4fb29c14537c523b512cea2f6c28f3b9bc6589c
Hideki Tanaka <work@tuxsnct.com>
2022-10-04 10:29:52 +0900

    fix: not refreshed when recommend data posted

--- App/lib/components/parent/data_delete.dart
+++ App/lib/components/parent/data_delete.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:solimage/routes/parent/history.dart';
 import 'package:solimage/utils/classes/expData.dart';
 import 'package:solimage/utils/classes/group.dart';
 import 'package:solimage/utils/classes/user.dart';
@@ -28,6 +29,7 @@ class DataDeleteDialog extends ConsumerWidget {
                             group?.removeExpData(expData.dataId);
                             await group?.update();
                           })));
+                      ref.refresh(recommendDataProvider);
                     }
                   }).then((_) {
                     ScaffoldMessenger.of(context).showSnackBar(
