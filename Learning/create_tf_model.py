73edb87f7e4669941f351a1d834a930c1ab9ec0b
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-10-04 09:23:11 +0900

    update: clr scale_fn

--- Learning/create_tf_model.py
+++ Learning/create_tf_model.py
@@ -1,7 +1,9 @@
-import os, json
+import matplotlib.pyplot as plt
+import os
+import json
 from statistics import mode
 
-#機械学習
+# 機械学習
 from keras.models import Model
 from keras.layers import Dense, Dropout, GlobalAveragePooling2D
 import numpy as np
@@ -25,12 +27,13 @@ dataPath = "image"
 basePath = os.path.dirname(os.path.abspath(__file__))
 dataPath = os.path.join(basePath, dataPath)
 
-train = ImageDataGenerator(rescale=1./255, # 255で割ることで正規化
-                           zoom_range=0.2, # ランダムにズーム
-                           horizontal_flip=True, # 水平反転
-                           rotation_range=40, # ランダムに回転
-                           vertical_flip=True) # 垂直反転
-trainGenerator = train.flow_from_directory(dataPath, target_size = (384, 216), batch_size = 16, class_mode = "categorical", shuffle=True)
+train = ImageDataGenerator(rescale=1./255,  # 255で割ることで正規化
+                           zoom_range=0.2,  # ランダムにズーム
+                           horizontal_flip=True,  # 水平反転
+                           rotation_range=40,  # ランダムに回転
+                           vertical_flip=True)  # 垂直反転
+trainGenerator = train.flow_from_directory(dataPath, target_size=(
+    384, 216), batch_size=16, class_mode="categorical", shuffle=True)
 
 labels = trainGenerator.class_indices
 os.makedirs("./tmp/model", exist_ok=True)
@@ -42,8 +45,8 @@ lebels_reverse = dict(zip(labels.values(), labels.keys()))
 json.dump(lebels_reverse, json_file)
 json_file.close()
 
-#layer構築
-#base_model = tf.keras.applications.EfficientNetV2L(
+# layer構築
+# base_model = tf.keras.applications.EfficientNetV2L(
 #    include_top=True,
 #    weights="imagenet",
 #    input_tensor=None,
@@ -52,45 +55,54 @@ json_file.close()
 #    classes=1000,
 #    classifier_activation="softmax",
 #    include_preprocessing=True,
-#)
+# )
 base_model = VGG16(include_top=False, weights='imagenet')
 x = base_model.output
 
 x = GlobalAveragePooling2D()(x)
 x = Dropout(0.5)(x)
-x = Dense(512, activation = tfa.activations.rrelu)(x)
+x = Dense(512, activation=tfa.activations.rrelu)(x)
 # 空間的なグローバルプーリング層を追加する
 #x = GlobalAveragePooling2D()(x)
 # 全結合層を追加する
 #x = Dense(512, activation='relu')(x)
 predictions = Dense(len(labels), activation="softmax")(x)
 
-#15層目までは再学習しないよう固定する
+# 15層目までは再学習しないよう固定する
 for layer in base_model.layers:
-  layer.trainable = False
+    layer.trainable = False
 
-#最適化アルゴリズムのimportと設定
-scale_fn = lambda x: tf.math.abs(tf.math.cos(x * np.pi / 6) - tf.math.sin(x * np.pi / 12)) / (tf.math.sqrt(tf.math.log(x + 1)) + tf.math.log(x + 1) + tf.math.abs(tf.sin(x * np.pi / 6)))
-clr = tfa.optimizers.CyclicalLearningRate(initial_learning_rate=1e-5, maximal_learning_rate=1e-3, step_size=512, scale_fn=scale_fn, scale_mode='cycle')
+# 最適化アルゴリズムのimportと設定
+
+
+def scale_fn(x): return tf.math.abs(tf.math.cos(x * np.pi / 6) - (tf.math.sin(x * np.pi / 24) * 0.5) + 1) /\
+    (tf.math.sqrt(tf.math.sqrt(tf.math.log(x + 1))) +
+     tf.math.sqrt(tf.math.log(x + 1)) + tf.math.abs(tf.sin(x * np.pi / 6)))
+
+
+clr = tfa.optimizers.CyclicalLearningRate(
+    initial_learning_rate=1e-5, maximal_learning_rate=1e-3, step_size=512, scale_fn=scale_fn, scale_mode='cycle')
 opt = adam_v2.Adam(clr)
 
-#モデルのコンパイルと詳細出力
-model = Model(inputs = base_model.input, outputs = predictions)
-model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=["accuracy"])
+# モデルのコンパイルと詳細出力
+model = Model(inputs=base_model.input, outputs=predictions)
+model.compile(optimizer=opt, loss='categorical_crossentropy',
+              metrics=["accuracy"])
 model.summary()
 
-early_stopping = EarlyStopping(monitor = "loss", patience = 5, min_delta = 1e-4)
-check_point = ModelCheckpoint("./tmp/model/model.h5", save_best_only = True, mode="min", monitor='loss')
+early_stopping = EarlyStopping(monitor="loss", patience=5, min_delta=1e-4)
+check_point = ModelCheckpoint(
+    "./tmp/model/model.h5", save_best_only=True, mode="min", monitor='loss')
 
-#学習
-history = model.fit(trainGenerator, epochs = 65, callbacks = [early_stopping, check_point])
+# 学習
+history = model.fit(trainGenerator, epochs=65, callbacks=[
+                    early_stopping, check_point])
 
 del model
 
 print("Learning Finished!")
 
-#学習結果表示
-import matplotlib.pyplot as plt
+# 学習結果表示
 fig = plt.figure()
 ax = fig.add_subplot(1, 2, 1)
 ax.plot(history.history['loss'], color='blue')
