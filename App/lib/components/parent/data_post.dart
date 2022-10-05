d660ecf534efc892783992aac395e44803d9fe8c
Hideki Tanaka <work@tuxsnct.com>
2022-10-05 15:28:51 +0900

    fix: expData list not refreshed

--- App/lib/components/parent/data_post.dart
+++ App/lib/components/parent/data_post.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:solimage/routes/parent/history.dart';
+import 'package:solimage/states/user.dart';
 import 'package:solimage/utils/classes/expData.dart';
 
 final _postingProvider = StateProvider.autoDispose((ref) => false);
@@ -42,6 +43,7 @@ class DataPostDialog extends ConsumerWidget {
                       if (expData.runtimeType == RecommendData) {
                         ref.refresh(recommendDataProvider);
                       }
+                      ref.refresh(userProvider);
                       context.go('/parent');
                     });
                   }
