50c1211421c5eb278a17e975db68b308eecb4a02
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-10-17 09:20:42 +0900

    add: preprocess script

---
+++ Learning/imgPreProcess.py
@@ -0,0 +1,52 @@
+import cv2 as cv
+import numpy as np
+import os
+from concurrent import futures
+
+basePath = os.path.dirname(os.path.abspath(__file__))
+imagesFolder = os.path.join(basePath, "image")
+
+
+def process(cat: str):
+    catPath = os.path.join(imagesFolder, cat)
+    fileNames = os.listdir(os.path.join(imagesFolder, cat))
+    fileNames = [file for file in fileNames if os.path.isfile(
+        os.path.join(imagesFolder, cat, file))]
+    WIDTH = 216
+    HEIGHT = 384
+    for file in fileNames:
+        imagePath = os.path.join(catPath, file)
+        writeRoot = "Croped"
+        writeDir = os.path.join(basePath, writeRoot, cat)
+        writePath = os.path.join(writeDir, file)
+
+        image = cv.imread(imagePath)
+        width = image.shape[1]
+        height = image.shape[0]
+        resizeRatio = min(WIDTH / width, HEIGHT / height)
+        image = cv.resize(image, (int(width * resizeRatio),
+                          int(height * resizeRatio)))
+        left = int((WIDTH - image.shape[1]) / 2)
+        right = WIDTH - image.shape[1] - left
+        top = int((HEIGHT - image.shape[0]) / 2)
+        bottom = HEIGHT - image.shape[0] - top
+        cropedImage = image[top:bottom, left:right]
+        cv.imwrite(writePath, cropedImage)
+
+
+def main():
+    categories = os.listdir(imagesFolder)
+    categories = [cat for cat in categories if os.path.isdir(
+        os.path.join(imagesFolder, cat))]
+
+    with futures.ProcessPoolExecutor(max_workers=24) as executor:
+        futureList = []
+        for cat in categories:
+            futureList.append(executor.submit(process, cat))
+
+        _ = futures.as_completed(futureList)
+    return
+
+
+if __name__ == "__main__":
+    main()
