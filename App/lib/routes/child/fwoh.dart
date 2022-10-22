25ca495160545bb7ed46088c1151641865aa291e
Hideki Tanaka <work@tuxsnct.com>
2022-10-22 09:39:36 +0900

    add: good and bad button

--- App/lib/routes/child/fwoh.dart
+++ App/lib/routes/child/fwoh.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:solimage/components/child/fwoh_card.dart';
+import 'package:solimage/states/user.dart';
 import 'package:solimage/utils/classes/expData.dart';
 
 final List<String> _cardLabels = [
@@ -10,14 +12,24 @@ final List<String> _cardLabels = [
   'だれ',
   'どうやって',
 ];
+final _goodProvider = FutureProvider.family<bool, int>((ref, dataId) async {
+  final user = await ref.watch(userProvider.future);
+  return user!.goodDatas.contains(dataId);
+});
+final _badProvider = FutureProvider.family<bool, int>((ref, dataId) async {
+  final user = await ref.watch(userProvider.future);
+  return user!.badDatas.contains(dataId);
+});
 
-class FWOHScreen extends StatelessWidget {
+class FWOHScreen extends ConsumerWidget {
   const FWOHScreen({Key? key, required this.data}) : super(key: key);
 
   final ExpData data;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final good = ref.watch(_goodProvider(data.dataId));
+    final bad = ref.watch(_badProvider(data.dataId));
     final cardDescriptions = [
       data.why,
       data.what,
@@ -37,26 +49,52 @@ class FWOHScreen extends StatelessWidget {
                       word: data.word!,
                       label: _cardLabels[index],
                       description: cardDescriptions[index]))),
-          FittedBox(
-              fit: BoxFit.contain,
-              child: Wrap(spacing: 10.0, children: [
-                ElevatedButton.icon(
-                    style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.all(30.0),
-                        textStyle: const TextStyle(
-                            fontSize: 30.0, fontWeight: FontWeight.bold)),
-                    label: const Text('おもしろい'),
-                    icon: const Icon(Icons.thumb_up, size: 50.0),
-                    onPressed: () {}),
-                ElevatedButton.icon(
-                    style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.all(30.0),
-                        textStyle: const TextStyle(
-                            fontSize: 30.0, fontWeight: FontWeight.bold)),
-                    label: const Text('つまらない'),
-                    icon: const Icon(Icons.thumb_down, size: 50.0),
-                    onPressed: () {}),
-              ]))
+          if (data is! RecommendData)
+            Row(children: [
+              Expanded(
+                  child: SizedBox.fromSize(
+                      size: const Size.fromHeight(100.0),
+                      child: good.maybeWhen(
+                          data: (good) => ElevatedButton.icon(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: good ? Colors.green : null,
+                                  padding: const EdgeInsets.all(20.0),
+                                  textStyle: const TextStyle(
+                                      fontSize: 30.0,
+                                      fontWeight: FontWeight.bold)),
+                              label: const FittedBox(
+                                  fit: BoxFit.contain, child: Text('おもしろい')),
+                              icon: const Icon(Icons.thumb_up, size: 30.0),
+                              onPressed: () async {
+                                final user =
+                                    await ref.read(userProvider.future);
+                                data.good(user!.uid);
+                              }),
+                          orElse: () => const Center(
+                              child: CircularProgressIndicator())))),
+              const SizedBox(width: 10.0),
+              Expanded(
+                  child: SizedBox.fromSize(
+                      size: const Size.fromHeight(100.0),
+                      child: bad.maybeWhen(
+                          data: (bad) => ElevatedButton.icon(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: bad ? Colors.red : null,
+                                  padding: const EdgeInsets.all(20.0),
+                                  textStyle: const TextStyle(
+                                      fontSize: 30.0,
+                                      fontWeight: FontWeight.bold)),
+                              label: const FittedBox(
+                                  fit: BoxFit.contain, child: Text('つまらない')),
+                              icon: const Icon(Icons.thumb_down, size: 30.0),
+                              onPressed: () async {
+                                final user =
+                                    await ref.read(userProvider.future);
+                                data.bad(user!.uid);
+                              }),
+                          orElse: () => const Center(
+                              child: CircularProgressIndicator()))))
+            ])
         ]));
   }
 }
