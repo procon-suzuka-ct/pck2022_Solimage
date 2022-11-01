22fa36cb0753fdaa9305ba7d9fbc83654c8a812f
Hideki Tanaka <work@tuxsnct.com>
2022-11-01 10:10:22 +0900

    update: redesigned standby dialog

--- App/lib/components/child/standby_dialog.dart
+++ App/lib/components/child/standby_dialog.dart
@@ -10,6 +10,7 @@ import 'package:solimage/components/child/child_actions.dart';
 import 'package:solimage/states/camera.dart';
 import 'package:solimage/states/user.dart';
 import 'package:solimage/utils/classes/expData.dart';
+import 'package:solimage/utils/classes/user.dart';
 import 'package:solimage/utils/imageProcess/classifier.dart';
 
 final _recommendDataProvider = FutureProvider.autoDispose((ref) async {
@@ -26,6 +27,14 @@ final _recommendDataProvider = FutureProvider.autoDispose((ref) async {
   }
 
   return recommendData;
+});
+
+final _recommendUserProvider = FutureProvider.autoDispose((ref) async {
+  final recommendData = await ref.watch(_recommendDataProvider.future);
+  if (recommendData != null) {
+    return AppUser.getUser(recommendData.userId);
+  }
+  return null;
 });
 
 final _labelsProvider =
@@ -57,110 +66,93 @@ class StandbyDialog extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final recommendData = ref.watch(_recommendDataProvider);
+    final recommendUser = ref.watch(_recommendUserProvider);
     final labels = ref.watch(_labelsProvider);
     final controller = PageController();
     final currentPage = ref.watch(_currentPageProvider);
+    final pages = [
+      recommendData.value != null && recommendUser.value != null
+          ? AlertDialog(
+              title: Center(
+                  child: Text(recommendData.value!.word,
+                      style: const TextStyle(
+                          fontSize: 30, fontWeight: FontWeight.bold))),
+              content: Column(mainAxisSize: MainAxisSize.min, children: [
+                Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: Text("${recommendUser.value!.name}さんからおすすめされました",
+                        style: const TextStyle(fontSize: 20))),
+                Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: AspectRatio(
+                        aspectRatio: 1,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: CachedNetworkImage(
+                                fit: BoxFit.cover,
+                                imageUrl: recommendData.value!.imageUrl!)))),
+                ChildActionButton(
+                    onPressed: () {
+                      HapticFeedback.heavyImpact();
+                      ref.read(imagePathProvider.notifier).state = '';
+                      Navigator.of(context).pop();
+                      context.push(
+                          '/child/result?userId=${recommendData.value!.userId}');
+                    },
+                    child: const Text('くわしく'))
+              ]))
+          : const SizedBox.shrink(),
+      AlertDialog(
+          title: const Center(
+              child: Text('どれだろう?',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold))),
+          content: labels.maybeWhen(
+              data: (labels) => SizedBox(
+                  width: 300.0,
+                  child: GridView.count(
+                      shrinkWrap: true,
+                      crossAxisCount: 2,
+                      children: labels.entries
+                          .map((label) => Card(
+                              child: InkWell(
+                                  customBorder: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  onTap: () {
+                                    HapticFeedback.heavyImpact();
+                                    ref.read(imagePathProvider.notifier).state =
+                                        '';
+                                    Navigator.of(context).pop();
+                                    context.push(
+                                        '/child/result?word=${label.key}');
+                                  },
+                                  child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      child: label.value.imageUrl!
+                                              .startsWith('data')
+                                          ? Image.memory(
+                                              UriData.parse(
+                                                      label.value.imageUrl!)
+                                                  .contentAsBytes(),
+                                              fit: BoxFit.cover)
+                                          : CachedNetworkImage(
+                                              imageUrl: label.value.imageUrl!,
+                                              fit: BoxFit.cover)))))
+                          .toList())),
+              orElse: () => const Center(
+                  heightFactor: 1.0, child: CircularProgressIndicator())))
+    ];
+    pages.removeWhere((element) => element is SizedBox);
 
     return Column(mainAxisSize: MainAxisSize.max, children: [
       Expanded(
           child: recommendData.maybeWhen(
               data: (data) => PageView(
-                      physics: const NeverScrollableScrollPhysics(),
-                      controller: controller,
-                      onPageChanged: (page) =>
-                          ref.read(_currentPageProvider.notifier).state = page,
-                      children: [
-                        AlertDialog(
-                            title: Center(
-                                child: Text(
-                                    data != null ? data.word : 'けっかをみてみよう!',
-                                    style: const TextStyle(
-                                        fontSize: 30,
-                                        fontWeight: FontWeight.bold))),
-                            content: data != null
-                                ? Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                        Padding(
-                                            padding: const EdgeInsets.only(
-                                                bottom: 10.0),
-                                            child: AspectRatio(
-                                                aspectRatio: 1,
-                                                child: ClipRRect(
-                                                    borderRadius:
-                                                        BorderRadius.circular(
-                                                            20.0),
-                                                    child: CachedNetworkImage(
-                                                        fit: BoxFit.cover,
-                                                        imageUrl:
-                                                            data.imageUrl!)))),
-                                        ChildActionButton(
-                                            onPressed: () {
-                                              HapticFeedback.heavyImpact();
-                                              ref
-                                                  .read(imagePathProvider
-                                                      .notifier)
-                                                  .state = '';
-                                              Navigator.of(context).pop();
-                                              context.push(
-                                                  '/child/result?userId=${data.userId}');
-                                            },
-                                            child: const Text('くわしく'))
-                                      ])
-                                : null),
-                        AlertDialog(
-                            title: const Center(
-                                child: Text('どれだろう?',
-                                    style: TextStyle(
-                                        fontSize: 30,
-                                        fontWeight: FontWeight.bold))),
-                            content: labels.maybeWhen(
-                                data: (labels) => SizedBox(
-                                    width: 300.0,
-                                    child: GridView.count(
-                                        shrinkWrap: true,
-                                        crossAxisCount: 2,
-                                        children: labels.entries
-                                            .map((label) => Card(
-                                                child: InkWell(
-                                                    customBorder:
-                                                        RoundedRectangleBorder(
-                                                      borderRadius:
-                                                          BorderRadius.circular(
-                                                              10.0),
-                                                    ),
-                                                    onTap: () {
-                                                      HapticFeedback
-                                                          .heavyImpact();
-                                                      ref
-                                                          .read(
-                                                              imagePathProvider
-                                                                  .notifier)
-                                                          .state = '';
-                                                      Navigator.of(context)
-                                                          .pop();
-                                                      context.push(
-                                                          '/child/result?word=${label.key}');
-                                                    },
-                                                    child: ClipRRect(
-                                                        borderRadius:
-                                                            BorderRadius.circular(
-                                                                10.0),
-                                                        child: label
-                                                                .value.imageUrl!
-                                                                .startsWith(
-                                                                    'data')
-                                                            ? Image.memory(
-                                                                UriData.parse(label.value.imageUrl!)
-                                                                    .contentAsBytes(),
-                                                                fit: BoxFit
-                                                                    .cover)
-                                                            : CachedNetworkImage(
-                                                                imageUrl: label.value.imageUrl!,
-                                                                fit: BoxFit.cover)))))
-                                            .toList())),
-                                orElse: () => const Center(child: CircularProgressIndicator())))
-                      ]),
+                  physics: const NeverScrollableScrollPhysics(),
+                  controller: controller,
+                  onPageChanged: (page) =>
+                      ref.read(_currentPageProvider.notifier).state = page,
+                  children: pages),
               orElse: () => const AlertDialog(
                   content: Center(
                       heightFactor: 1.0, child: CircularProgressIndicator())))),
@@ -169,7 +161,9 @@ class StandbyDialog extends ConsumerWidget {
             child: const Text('もどる'),
             onPressed: () => Navigator.of(context).pop()),
         labels.maybeWhen(
-            data: (data) => currentPage == 0
+            data: (data) => currentPage == 0 &&
+                    recommendData.value != null &&
+                    recommendUser.value != null
                 ? ChildActionButton(
                     onPressed: () {
                       HapticFeedback.heavyImpact();
