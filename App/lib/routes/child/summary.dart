b5e657b706b14526adfa816db6fa9b2c904245a9
Hideki Tanaka <work@tuxsnct.com>
2022-11-03 14:52:16 +0900

    fix: broken layout

--- App/lib/routes/child/summary.dart
+++ App/lib/routes/child/summary.dart
@@ -8,41 +8,34 @@ class SummaryScreen extends StatelessWidget {
   final ExpData data;
 
   @override
-  Widget build(BuildContext context) =>
-      Wrap(
-          direction: Axis.vertical,
-          alignment: WrapAlignment.center,
-          runAlignment: WrapAlignment.center,
-          crossAxisAlignment: WrapCrossAlignment.center,
-          spacing: 10.0,
+  Widget build(BuildContext context) => Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(data.word,
-                style: const TextStyle(
-                    fontSize: 40.0, fontWeight: FontWeight.bold)),
-            ConstrainedBox(
-                constraints: BoxConstraints(
-                    maxWidth: MediaQuery
-                        .of(context)
-                        .size
-                        .width - 40),
+            Padding(
+                padding: const EdgeInsets.only(bottom: 10.0),
+                child: Text(data.word,
+                    style: const TextStyle(
+                        fontSize: 40.0, fontWeight: FontWeight.bold))),
+            Expanded(
                 child: data.imageUrl != null
                     ? ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0),
-                    child: data.imageUrl!.startsWith('data')
-                        ? Image.memory(
-                        UriData.parse(data.imageUrl!).contentAsBytes(),
-                        fit: BoxFit.contain)
-                        : CachedNetworkImage(
-                        imageUrl: data.imageUrl!,
-                        fit: BoxFit.contain,
-                        placeholder: (context, url) =>
-                        const Center(
-                            child: CircularProgressIndicator()),
-                        errorWidget: (context, url, error) =>
-                        const Icon(
-                            Icons
-                                .signal_wifi_statusbar_connected_no_internet_4,
-                            size: 60.0)))
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: data.imageUrl!.startsWith('data')
+                            ? Image.memory(
+                                UriData.parse(data.imageUrl!).contentAsBytes(),
+                                fit: BoxFit.contain)
+                            : CachedNetworkImage(
+                                imageUrl: data.imageUrl!,
+                                fit: BoxFit.contain,
+                                placeholder: (context, url) => const Center(
+                                    child: CircularProgressIndicator()),
+                                errorWidget: (context, url, error) => const Icon(
+                                    Icons
+                                        .signal_wifi_statusbar_connected_no_internet_4,
+                                    size: 60.0)))
                     : const Icon(Icons.no_photography, size: 80.0))
-          ]);
+          ]));
 }
